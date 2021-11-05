56e6678d7974747c7d089c7ab6e8ea3cc89b2c00
SzejkerPM <81755515+SzejkerPM@users.noreply.github.com>
2021-11-05 13:40:07 +0100

    Initial Commit

---
+++ src/main/java/pl/mw/cvenchanted/CvEnchantedApplication.java
@@ -0,0 +1,13 @@
+package pl.mw.cvenchanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvEnchantedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvEnchantedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mw/cvenchanted/CvEnchantedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mw.cvenchanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvEnchantedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
